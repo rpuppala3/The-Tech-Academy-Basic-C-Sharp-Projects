3a9361e4c406fc6c0cc1dadd2d7e2b34e2f1c380
rpuppala3 <rpuppala3@gmail.com>
2025-11-20 12:49:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        Console.WriteLine("Please enter the package weight:");
+        int weight = Convert.ToInt32(Console.ReadLine());
+
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            return;
+        }
+
+        Console.WriteLine("Please enter the package width:");
+        int width = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter the package height:");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please enter the package length:");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        if (width + height + length > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+            return;
+        }
+
+        double quote = ((width * height * length) * weight) / 100.0;
+        Console.WriteLine("Your estimated total for shipping this package is: ${0:F2}", quote);
+
+        Console.WriteLine("Thank you!");
+    }
+}
